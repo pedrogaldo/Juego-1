5268d64da6e5a3f837566676626253ee810c607b
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2023-06-13 15:48:36 -0300

    Juego1
    Empezando script de controles para tambores

--- Assets/scripts/Controls.cs
+++ Assets/scripts/Controls.cs
@@ -1,16 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Controls : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            GetComponent<Renderer>().enabled = true;
+        }
+
+
+
+        if (Input.GetKey(KeyCode.X))
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+
+
+
+    }
 }
