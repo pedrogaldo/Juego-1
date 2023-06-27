baa37c302d342ee7c6ab12447a0361a0ebcb6df8
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2023-06-27 15:57:08 -0300

    .

--- Assets/scripts/pausa.cs
+++ Assets/scripts/pausa.cs
@@ -2,8 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class pausa : MonoBehaviour
 {
+
+    public static bool pausado = false;
+    public GameObject menuPausa;
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +16,41 @@ public class pausa : MonoBehaviour
     }
 
     // Update is called once per frame
+
+
+
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            if (pausado)
+            {
+                Resumir();
+            }
+
+            else
+            {
+                Pausar();
+            }
+        }
+
+
     }
+
+    void Resumir()
+    {
+        menuPausa.SetActive(false);
+        Time.timeScale = 1f;
+        pausado = false;
+    }
+
+
+    void Pausar()
+    {
+        menuPausa.SetActive(true);
+        Time.timeScale = 0f;
+        pausado = true;
+    }
+
+
 }
